8b027d977e9848c1bd6249eb70d65e031c157907
Gokul-git4 <119651989+Gokul-git4@users.noreply.github.com>
2023-03-28 09:39:13 +0530

    Update application-stop.sh

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-#echo "stopping go application"
-#ps -ef | grep main| grep -v grep | awk '{print $2}' | xargs kill
+!/bin/bash
+echo "stopping go application"
+ps -ef | grep main| grep -v grep | awk '{print $2}' | xargs kill
